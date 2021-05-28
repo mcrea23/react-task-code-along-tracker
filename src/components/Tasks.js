ef7b136fa2476c6c7616d532a9519ea3016d12a3
+ <git-commit-mcreasmith23@gmail.com>
2021-05-28 13:55:29 -0500

    components/Tasks:  removed array of taks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,4 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'June 1st at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting',
-    day: 'June 14th at 1:00pm',
-    reminder: true,
-  },  
-  {
-    id: 3,
-    text: 'Hiking',
-    day: 'June 10th at 11:00am',
-    reminder: false,
-  },
 
-]
 const Tasks = () => {
   return (
     <>
