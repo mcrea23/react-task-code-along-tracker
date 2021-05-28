6c88c3b589c0119d2e101c6ef07065bf1f127253
+ <git-commit-mcreasmith23@gmail.com>
2021-05-28 14:13:04 -0500

    src: set task state to top level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'June 1st at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting',
+      day: 'June 14th at 1:00pm',
+      reminder: true,
+    },  
+    {
+      id: 3,
+      text: 'Hiking',
+      day: 'June 10th at 11:00am',
+      reminder: false,
+    },
+  ])
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,4 @@
-import { useState} from 'react'
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'June 1st at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting',
-      day: 'June 14th at 1:00pm',
-      reminder: true,
-    },  
-    {
-      id: 3,
-      text: 'Hiking',
-      day: 'June 10th at 11:00am',
-      reminder: false,
-    },
-  ])
+const Tasks = ({ tasks }) => {
   return (
     <>
     {tasks.map((task) => 
